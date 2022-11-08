a03d718a42179a644fc28601dfa1aa26a49783c9
Matthew Cain <codingdatum@gmail.com>
2022-11-08 00:09:29 -0500

    added button functionality for current throw

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Main.module.css';
 
 const Main = props => {
+
+    const [whiteTeamWorkingPoints, setWhiteTeamWorkingPoints] = useState(0);
+    const [redTeamWorkingPoints, setRedTeamWorkingPoints] = useState(0);
+
+    const addWhitePoint = () => {
+        setWhiteTeamWorkingPoints(whiteTeamWorkingPoints+1)
+    }
+    const subtractWhitePoint = () => {
+        setWhiteTeamWorkingPoints(whiteTeamWorkingPoints-1)
+    }
+    const addRedPoint = () => {
+        setRedTeamWorkingPoints(redTeamWorkingPoints+1)
+    }
+    const subtractRedPoint = () => {
+        setRedTeamWorkingPoints(redTeamWorkingPoints-1)
+    }
+
     return (
         <div className={styles.main}>
             <div className={styles["white-team-container"]}>
                 <h3>points</h3>
                 <div className={styles["point-buttons"]}>
-                    <button>+</button>
-                    <span>0</span>
-                    <button>-</button>
+                    <button onClick={addWhitePoint}>+</button>
+                    <span>{whiteTeamWorkingPoints}</span>
+                    <button onClick={subtractWhitePoint}>-</button>
                 </div>
                 <h2>WHITE TEAM</h2>
             </div>
             <div className={styles["red-team-container"]}>
                 <h3>points</h3>
                 <div className={styles["point-buttons"]}>
-                    <button>+</button>
-                    <span>2</span>
-                    <button>-</button>
+                    <button onClick={addRedPoint}>+</button>
+                    <span>{redTeamWorkingPoints}</span>
+                    <button onClick={subtractRedPoint}>-</button>
                 </div>
                 <h2>RED TEAM</h2>
             </div>
