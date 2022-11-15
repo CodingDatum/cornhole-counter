0d9a1ca7c206c48d5514a32916f25c5ddcaa0f07
Matthew Cain <codingdatum@gmail.com>
2022-11-15 18:45:45 -0500

    put in rules

--- src/components/Navbar/GameRules.js
+++ src/components/Navbar/GameRules.js
@@ -7,8 +7,13 @@ const GameRules = props => {
     return (
         <Backdrop>
             <div className={styles.rules}>
-                <p>Game Rules</p>
-                <button onClick={props.closeRules}>close</button>
+                <h3>Game Rules</h3>
+                <p>1. Stand on the opposite board to your partner</p>
+                <p>2. Throw your bag (underhand) at the opposite board. Alternate throws with the player next to you.</p>
+                <p>3. 1 POINT for each bag that lands on top of the board. 3 POINTS for each bag that is tossed through or knocked into the hole.</p>
+                <p>4. Tally points after all (8) bags have been thrown per inning by using the cancellation method. TOTAL = YOUR POINTS - OPPONENT'S POINTS</p>
+                <p>5. The game is over when a Player or Team reaches (or exceeds) 21 POINTS.</p>
+                <button onClick={props.closeRules}>x</button>
             </div>
         </Backdrop>
     )
